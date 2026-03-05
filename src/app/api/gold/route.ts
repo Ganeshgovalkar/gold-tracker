121b59bddb95614edf377231e3c0019375241793
Ganesh Govalkar <ganeshgovalkar@GaneshGovalkar-MBP.local>
2026-03-05 18:42:43 +0530

    add gold api route

---
+++ src/app/api/gold/route.ts
@@ -0,0 +1,15 @@
+export async function GET() {
+    try {
+      const res = await fetch("https://api.gold-api.com/price/XAU");
+  
+      const data = await res.json();
+  
+      return Response.json(data);
+  
+    } catch (error) {
+      return Response.json(
+        { error: "Failed to fetch gold price" },
+        { status: 500 }
+      );
+    }
+  }
